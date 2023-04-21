0461c27e49ab5f06a11410c6c61a2b2006098142
Philipp Belitz <philipp.belitz@securesystems.de>
2023-04-21 11:38:55 +0200

    feat: helm chart and custom rules
    Switched installation to helm chats. Additionally, custom semgrep rules can be added to the helm chart at `helm/rules`.

---
+++ semgr8s/__main__.py
@@ -0,0 +1,24 @@
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from semgr8s.app import APP
+from semgr8s.updater import update_rules
+
+if __name__ == "__main__":
+    APP.logger.addHandler(logging.StreamHandler())
+    APP.logger.setLevel(logging.DEBUG)
+
+    scheduler = BackgroundScheduler()
+    job = scheduler.add_job(update_rules, "interval", minutes=1)
+    scheduler.start()
+
+    # first run at start up
+    update_rules()
+
+    APP.run(
+        ssl_context=("/etc/ssl/ca.crt", "/etc/ssl/ca.key"),
+        port=5000,
+        host="0.0.0.0",
+        debug=True,
+    )
--- templates/secret.tpl
+++
@@ -1,9 +0,0 @@
----
-apiVersion: v1
-kind: Secret
-metadata:
-  name: __WEBHOOK__-secret
-  namespace: __NAMESPACE__
-data:
-  ca.crt: __CRT__
-  ca.key: __KEY__
