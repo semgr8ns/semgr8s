9084cf59efc8387946e756a7238c545a54966a39
Christoph Hamsen <hamsen.christoph@posteo.de>
2024-02-08 17:15:41 +0100

    build: poetry for pinned python versions
    fix
    
    fix
    
    fix

--- build/harden.sh
+++ build/harden.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 set -euo pipefail
 
-# Update packages and remove apk
-apk update --no-cache && apk upgrade --no-cache && apk del apk-tools --no-cache && rm -rf /var/cache/apk
+# Update packages
+#apk update --no-cache && apk upgrade --no-cache && rm -rf /var/cache/apk
+
+# Remove apk
+find / -type f -iname '*apk*' -xdev -delete
+find / -type d -iname '*apk*' -print0 -xdev | xargs -0 rm -r --
 
 # Remove pip
 pip uninstall pip --yes
 
 # Remove user accounts
-echo "" > /etc/group
-echo "" > /etc/passwd
-echo "" > /etc/shadow
+echo "" >/etc/group
+echo "" >/etc/passwd
+echo "" >/etc/shadow
 
 # Remove crons
 rm -fr /var/spool/cron
@@ -25,3 +29,6 @@ rm -f /etc/inittab
 # Remove media stuff
 rm -f /etc/fstab
 rm -fr /media
+
+# Remove this file
+rm "$0"
