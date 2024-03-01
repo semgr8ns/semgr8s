22b726c039dec2ba64cc373cd15d6758f4e2197a
Christoph Hamsen <hamsen.christoph@posteo.de>
2024-03-01 10:15:46 +0100

    feat: improved admission response format

--- semgr8s/app.py
+++ semgr8s/app.py
@@ -10,6 +10,7 @@ import yaml
 from flask import Flask, jsonify, request
 from semgrep.cli import cli
 from werkzeug.utils import secure_filename
+from werkzeug.exceptions import UnsupportedMediaType
 
 APP = Flask(__name__)
 DATA_FOLDER = "/app/data"
@@ -24,7 +25,7 @@ def healthz():
     return "", 200
 
 
-@APP.route("/ready", methods=["GET"])
+@APP.route("/ready", methods=["GET", "POST"])
 def readyz():
     """
     Handle the '/ready' endpoint and check the readiness of the web server.
@@ -38,22 +39,51 @@ def validate():
     """
     Handle the '/validate' endpoint and accept admission rquests.
     Return response allowing or denying the request.
+
+    Return '200' status code with response:
+    {
+        'apiVersion': 'admission.k8s.io/v1',
+        'kind': 'AdmissionReview',
+        'response': {
+            'allowed': <bool True/False>,
+            'status': {
+                'code': <int http_status_codes>,
+                'message': <str reponse_message>
+            },
+            'uid': <str admission_request_uid>
+        }
+    }
+
+    Response payload status codes:
+    * 201 - Admitted
+    * 400 - Malformed request payload lacking 'request' key/values
+    * 403 - Request violates policy rules
+    * 415 - Unsupported request payload media type
+    * 418 - Error during semgrep scan
+    * 422 - Malformed request payload without 'request.uid' key
+    * 500 - Unexpected Webhook exception
+
     """
     uid = ""
 
     try:
-        req = (request.json).get("request", {})
+        APP.logger.debug("+ request object: %s", request)
+        try:
+            req = (request.json).get("request", {})
+        except (UnsupportedMediaType, AttributeError) as err:
+            return send_response(False, "none", 415, f"Unsupported Media Type: {err}")
+
         APP.logger.debug("+ request: %s", req)
 
         if not req:
             return send_response(
-                False, "none", "Invalid request, no payload.request found"
+                False, "none", 400, "Malformed request, no payload.request found"
             )
 
         uid = req.get("uid", "")
         if not uid:
             return send_response(
-                False, "none", "Invalid request, no payload.request.uid found"
+                False, "none", 422, "Malformed request, no payload.request.uid found"
             )
         k8s_yaml_file = os.path.join(DATA_FOLDER, secure_filename(f"k8s_{uid}.yml"))
         results_file = os.path.join(DATA_FOLDER, secure_filename(f"results_{uid}.json"))
@@ -86,7 +116,7 @@ def validate():
         try:
             cli()  # pylint: disable=no-value-for-parameter
         except SystemExit as err:
-            APP.logger.debug("+ semgrep exit: %s", err)
+            APP.logger.info("+ Unexpected semgrep error recorded: %s", err)
 
         with open(results_file, "r", encoding="utf-8") as file:
             results = json.load(file)
@@ -94,7 +124,7 @@ def validate():
 
         if results["errors"]:
             APP.logger.error("ERROR: %s", results["errors"])
-            return send_response(True, uid, "Request caused error during scan")
+            return send_response(False, uid, 418, "Request caused error during semgrep scan")
 
         if results["results"]:
             num_findings = len(results["results"])
@@ -105,26 +135,31 @@ def validate():
             return send_response(
                 False,
                 uid,
+                403,
                 f"Found {num_findings} violation(s) of the following policies: {findings}",
             )
     except Exception as err:  # pylint: disable=W0718
-        return send_response(False, uid, f"Webhook exception: {err}")
+        return send_response(False, uid, 500, f"Unexpected Webhook exception: {err}")
     finally:
         try:
             os.remove(results_file)
             os.remove(k8s_yaml_file)
-        except FileNotFoundError:
+        except (FileNotFoundError, UnboundLocalError):
             pass
 
-    return send_response(True, uid, "Compliant resource admitted")
+    return send_response(True, uid, 201, "Compliant resource admitted")
 
 
-def send_response(allowed, uid, message):
+def send_response(allowed, uid, code, message):
     """
     Prepare json response in expected format based on validation result.
     """
-    APP.logger.debug(
-        "> response:(allowed=%s, uid=%s, message=%s)", allowed, uid, message
+    APP.logger.info(
+        "> response:(allowed=%s, uid=%s, status_code=%s message=%s)",
+        allowed,
+        uid,
+        code,
+        message,
     )
     return jsonify(
         {
@@ -133,7 +168,7 @@ def send_response(allowed, uid, message):
             "response": {
                 "allowed": allowed,
                 "uid": uid,
-                "status": {"message": message},
+                "status": {"code": code, "message": message},
             },
         }
     )
