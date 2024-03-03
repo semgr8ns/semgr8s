5efbaa2d37d4f80489fad126bc107c136812950b
Christoph Hamsen <hamsen.christoph@posteo.de>
2024-03-03 21:17:54 +0100

    feat: add autofix via mutation
    fix
    
    build: bla
    
    fix
    
    fix
    
    fix
    
    fix test

--- semgr8s/app.py
+++ semgr8s/app.py
@@ -2,10 +2,14 @@
 Flask application to expose required paths and generate suitable responses.
 """
 
+import base64
 import json
 import os
+import random
+import string
 import sys
 
+import jsonpatch
 import yaml
 from flask import Flask, jsonify, request
 from semgrep.cli import cli
@@ -14,6 +18,7 @@ from werkzeug.exceptions import UnsupportedMediaType
 
 APP = Flask(__name__)
 DATA_FOLDER = "/app/data"
+RANDOM = random.SystemRandom()
 
 
 @APP.route("/health", methods=["GET", "POST"])
@@ -35,10 +40,31 @@ def readyz():
 
 
 @APP.route("/validate", methods=["POST"])
-def validate():  # pylint: disable=too-many-return-statements
+def validate():
     """
-    Handle the '/validate' endpoint and accept admission rquests.
+    Handle the '/validate' endpoint and accept admission requests.
     Return response allowing or denying the request.
+    """
+
+    return perform_review(request, autofix=False)
+
+
+@APP.route("/mutate", methods=["POST"])
+def mutate():
+    """
+    Handle the '/mutate' endpoint and accept admission requests.
+    Return response allowing, denying, or modifying the request.
+    """
+
+    return perform_review(request, autofix=True)
+
+
+def perform_review(
+    admission_request, autofix=False
+):  # pylint: disable=too-many-return-statements
+    """
+    Review admission request using semgrep as policy engine.
+    Return admission response.
 
     Return '200' status code with response:
     {
@@ -56,20 +82,21 @@ def validate():  # pylint: disable=too-many-return-statements
 
     Response payload status codes:
     * 201 - Admitted
+    * 202 - Passed mutation phase, validation still open
     * 400 - Malformed request payload lacking 'request' key/values
     * 403 - Request violates policy rules
     * 415 - Unsupported request payload media type
     * 418 - Error during semgrep scan
     * 422 - Malformed request payload without 'request.uid' key
     * 500 - Unexpected Webhook exception
-
     """
     uid = ""
+    postfix = "".join(RANDOM.choices(string.ascii_letters + string.digits, k=10))
 
     try:
-        APP.logger.debug("+ request object: %s", request)
+        APP.logger.debug("+ request object: %s", admission_request)
         try:
-            req = (request.json).get("request", {})
+            req = (admission_request.json).get("request", {})
         except (UnsupportedMediaType, AttributeError) as err:
             return send_response(False, "none", 415, f"Unsupported Media Type: {err}")
 
@@ -85,12 +112,16 @@ def validate():  # pylint: disable=too-many-return-statements
             return send_response(
                 False, "none", 422, "Malformed request, no payload.request.uid found"
             )
-        k8s_yaml_file = os.path.join(DATA_FOLDER, secure_filename(f"k8s_{uid}.yml"))
-        results_file = os.path.join(DATA_FOLDER, secure_filename(f"results_{uid}.json"))
+        k8s_yaml_file = os.path.join(
+            DATA_FOLDER, secure_filename(f"k8s_{uid}_{postfix}.yml")
+        )
+        results_file = os.path.join(
+            DATA_FOLDER, secure_filename(f"results_{uid}_{postfix}.json")
+        )
 
-        k8syaml = req.get("object", {})
+        k8s_res = req.get("object", {})
         with open(k8s_yaml_file, "w", encoding="utf-8") as file:
-            yaml.dump(k8syaml, file, default_flow_style=False)
+            yaml.safe_dump(k8s_res, file, default_flow_style=False)
 
         remote_rules = []
         for remote_rule in os.environ.get("SEMGREP_RULES", "").split(" "):
@@ -102,6 +133,7 @@ def validate():  # pylint: disable=too-many-return-statements
             "--metrics",
             "off",
             "--disable-version-check",
+            *(["--autofix"] if autofix else []),
             "--config",
             "/app/rules/",
             *remote_rules,
@@ -129,16 +161,23 @@ def validate():  # pylint: disable=too-many-return-statements
             )
 
         if results["results"]:
+            if autofix:
+                with open(k8s_yaml_file, "r", encoding="utf-8") as file:
+                    patched_k8s_res = yaml.safe_load(file)
+                patches = jsonpatch.JsonPatch.from_diff(k8s_res, patched_k8s_res).patch
+            else:
+                patches = None
             num_findings = len(results["results"])
             findings = "\n" + "\n".join(
                 ["* " + f["check_id"] for f in results["results"]]
             )
             APP.logger.debug("+ %s finding(s): %s", num_findings, findings)
             return send_response(
-                False,
+                autofix,
                 uid,
-                403,
+                202 if autofix else 403,
                 f"Found {num_findings} violation(s) of the following policies: {findings}",
+                patch=patches,
             )
     except Exception as err:  # pylint: disable=W0718
         return send_response(False, uid, 500, f"Unexpected Webhook exception: {err}")
@@ -152,7 +191,7 @@ def validate():  # pylint: disable=too-many-return-statements
     return send_response(True, uid, 201, "Compliant resource admitted")
 
 
-def send_response(allowed, uid, code, message):
+def send_response(allowed, uid, code, message, patch=None):
     """
     Prepare json response in expected format based on validation result.
     """
@@ -163,14 +202,19 @@ def send_response(allowed, uid, code, message):
         code,
         message,
     )
-    return jsonify(
-        {
-            "apiVersion": "admission.k8s.io/v1",
-            "kind": "AdmissionReview",
-            "response": {
-                "allowed": allowed,
-                "uid": uid,
-                "status": {"code": code, "message": message},
-            },
-        }
-    )
+    review = {
+        "apiVersion": "admission.k8s.io/v1",
+        "kind": "AdmissionReview",
+        "response": {
+            "allowed": allowed,
+            "uid": uid,
+            "status": {"code": code, "message": message},
+        },
+    }
+    if patch:
+        review["response"]["patchType"] = "JSONPatch"
+        review["response"]["patch"] = base64.b64encode(
+            bytearray(json.dumps(patch), "utf-8")
+        ).decode("utf-8")
+
+    return jsonify(review)
